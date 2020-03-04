f1252b6d7c792d32ca43e9a83a7e53c5843e1042
Guy Ohad <guy.ohad@weizmann.ac.il>
2020-03-04 07:26:00 -0800

    add Si to Modules/set_hubbard_l.f90, Modules/set_hubbard_n.f90 and PW/src/tabd.f90

--- PW/src/tabd.f90
+++ PW/src/tabd.f90
@@ -97,7 +97,7 @@ FUNCTION hubbard_occ ( psd )
      !
      ! OTHER ELEMENTS
      !
-     CASE( 'C'  )
+     CASE( 'C', 'Si'  )
         hubbard_occ = 2.d0
      !
      CASE( 'N'  )
@@ -111,7 +111,6 @@ FUNCTION hubbard_occ ( psd )
      !
      CASE( 'Ga', 'In'  )
         hubbard_occ = 10.d0
-     !
      !
      ! NOT INSERTED
      !
